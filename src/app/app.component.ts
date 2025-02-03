69f8ec008722222d24e59ac872c077eb81486793
AlexTix <tixaskiy@gmail.com>
2025-02-03 10:10:11 +0200

    Update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextBoxComponent } from "./components/text-box/text-box.component";
+import { InfoBoxComponent } from "./components/info-box/info-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextBoxComponent, InfoBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
