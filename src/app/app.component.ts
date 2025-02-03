fa57094509def1b2230a0ad2e9db2b2fc085c5eb
AlexTix <tixaskiy@gmail.com>
2025-02-03 10:39:32 +0200

    add new block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextBoxComponent } from "./components/text-box/text-box.component";
 import { InfoBoxComponent } from "./components/info-box/info-box.component";
+import { NewsBoxComponent } from "./components/news-box/news-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextBoxComponent, InfoBoxComponent],
+  imports: [RouterOutlet, TextBoxComponent, InfoBoxComponent, NewsBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/news-box/news-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-box.component.html',
+  styleUrl: './news-box.component.scss'
+})
+export class NewsBoxComponent {
+
+}
